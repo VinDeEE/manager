c682667f72cfdba32f5e8e16c310bb6b3a4788f9
wayne.wang <wayne.wang@accentrix.com>
2019-07-29 16:41:40 +0800

    fix bug

--- manager-api/src/main/java/com/vd/api/web/ControllerApi.java
+++ manager-api/src/main/java/com/vd/api/web/ControllerApi.java
@@ -1,14 +1,14 @@
 package com.vd.api.web;
 
-import com.vd.common.data.model.entity.Student;
+import com.vd.common.data.model.entity.Stu;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import java.util.List;
 
 @Api(value = "tast测试接口", description = "测试11接口")
 public interface ControllerApi {
     @ApiOperation("查找全部")
-    public List<Student> getAll();
+    public List<Stu> getAll();
 }
